42ed3b3517796ddb0bb01e8a1de0531fe3eb26cb
Darina <ivanno79@yandex.ru>
2024-02-12 18:43:58 +0300

    lab1 done

---
+++ lab1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UFT-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Лабораторная работа 1</title>
+</head>
+<body>
+    <header>
+        <img src="./image/poly_logo.png" alt="logo" width="30px" height="30px">
+        <h1 class="title">Домашнаяя работа "Hello, world!"</h1>
+    </header>
+    <main>
+        <?php
+            echo 'Hello, world!'
+        ?>
+    </main>
+    <footer>
+        <h2 class="subtitle">Создать веб-страницу с динамическим контентом.<br>Тарантина Дарина</h2>
+    </footer>
+</body>
+</html>
